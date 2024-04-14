88b75c1e7693e4e7e96435a5bdede085d53e6c99
Luan Souza <alunoluansenai@gmail.com>
2024-04-14 09:59:23 -0300

    🎉 Commit Inicial:
    Projeto da Origamid Cursos para aprendizado de Javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿let animais = document.getElementById("animais");
+console.log(animais);
+
+let animaisClass = document.getElementsByClassName("animais");
+
+console.log("Teste");
+console.log(animaisClass);
+
+let linksInternos = document.querySelector("[href^='#']");
+console.log(linksInternos);
