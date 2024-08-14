6b1734a500ec570c6017e090df04ed85eaccd6e4
Luan Souza <alunoluansenai@gmail.com>
2024-08-14 20:28:40 -0300

    Voltando com o curso de Javascript

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-﻿let animais = document.getElementById("animais");
-console.log(animais);
-
-let animaisClass = document.getElementsByClassName("animais");
-
-console.log("Teste");
-console.log(animaisClass);
-
-let linksInternos = document.querySelector("[href^='#']");
-console.log(linksInternos);
+﻿
